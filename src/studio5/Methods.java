1e266717cf7db7e806da65afa1c40b6213bf2ca4
snwobi23 <snwobi@brooksschool.org>
2025-10-15 14:51:08 -0500

    completed bullseyes

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -15,7 +15,6 @@ public class Methods {
 	 */
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
 		double distance = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1,2));
-
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
 		
 		return distance;
@@ -32,17 +31,21 @@ public class Methods {
 		StdDraw.setPenColor(StdDraw.BLACK);
 		StdDraw.filledCircle(x, y, radius);
 
+		StdDraw.setPenColor(0,109,219);
+		StdDraw.filledCircle(x,y,(3.0/4.0) * radius);
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
 
 		
-
+		StdDraw.setPenColor(146,0,0);
+		StdDraw.filledCircle(x,y,(1.0/2.0) * radius);
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
 
 		
-
+		StdDraw.setPenColor(255,255,109);
+		StdDraw.filledCircle(x,y,(1.0/4.0) * radius);
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
 
